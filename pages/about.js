1c1352607b8d985e29aa4b74d7bbed5050607449
Joseph Luk <joluk@paypal.com>
2018-12-02 14:13:47 -0800

    Add image tag.

--- pages/about.js
+++ pages/about.js
@@ -3,5 +3,6 @@ import Layout from '../components/MyLayout.js'
 export default () => (
     <Layout>
        <p>This is the about page</p>
+       <img src="./static/tango-beach.jpg"/>
     </Layout>
 )
